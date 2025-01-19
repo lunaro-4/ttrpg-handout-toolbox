c504f1a1cc784c1a39262e37d63430ddd914f9e9
lunaro-4 <pavelike@gmail.com>
2025-01-19 17:08:19 +0300

    add stat modification from data to template

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 from template_handler import TemplateHandler
+from spell_data_handler import SpellData
+
 
 
 def main():
 
+    sd = SpellData()
+    spell_data = sd.get_spell_json()
+
 
     th = TemplateHandler()
     soup = th.soup
-    components_value = soup.new_tag("p")
-    components_value.string = "123"
-    th.components.append(components_value)
+    components_element = soup.new_tag("p")
+    components_element.string = str(spell_data.get('components'))
+    duration_element = soup.new_tag("p")
+    duration_element.string = str(spell_data.get('duration'))
+    distance_element = soup.new_tag("p")
+    distance_element.string = str(spell_data.get('distance'))
+    casting_time_element = soup.new_tag("p")
+    casting_time_element.string = str(spell_data.get('casting_time'))
+    description_element = soup.new_tag("p")
+    description_element.string = str(spell_data.get('description'))
+
+
+    th.components.append(components_element)
+    th.duration.append(duration_element)
+    th.spell_range.append(distance_element)
+    th.casting_time.append(casting_time_element)
+    th.description.append(description_element)
 
     th.render()
 
