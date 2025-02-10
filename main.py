307beaafe5d62b79c97572f79ebb68e822bb3adc
lunaro <pavelike@gmail.com>
2025-02-10 16:47:03 +0300

    HACK: add symlink for src folder in template directory for html2png to resolve images correctly

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
 from template_handler import TemplateHandler
-from spell_data_handler import SpellData
+from spell_data_handler import Spell
 
+def convert_components_to_string(component_dict: dict[str, bool]) -> str:
+    return_list: list[str] = []
+    if component_dict.get('verbal'):
+        return_list.append('В')
+    if component_dict.get('somatic'):
+        return_list.append('С')
+    if component_dict.get('material'):
+        return_list.append('М')
+    return ','.join(return_list)
 
 
 def main():
 
-    sd = SpellData()
-    spell_data = sd.get_spell_json()
+    spell_data = Spell.load_from_json("spell_data_from_dndsu/Aid")
 
 
     th = TemplateHandler()
     soup = th.soup
     components_element = soup.new_tag("p")
-    components_element.string = str(spell_data.get('components'))
+    components_element.string = convert_components_to_string(spell_data.get_components())
     duration_element = soup.new_tag("p")
-    duration_element.string = str(spell_data.get('duration'))
+    duration_element.string = str(spell_data.get_duration())
     distance_element = soup.new_tag("p")
-    distance_element.string = str(spell_data.get('distance'))
+    distance_element.string = str(spell_data.get_distance())
     casting_time_element = soup.new_tag("p")
-    casting_time_element.string = str(spell_data.get('casting_time'))
+    casting_time_element.string = str(spell_data.get_casting_time())
     description_element = soup.new_tag("p")
-    description_element.string = str(spell_data.get('description'))
+    description_element.string = str(spell_data.get_description())
 
 
-    th.components.append(components_element)
-    th.duration.append(duration_element)
-    th.spell_range.append(distance_element)
-    th.casting_time.append(casting_time_element)
-    th.description.append(description_element)
+    th.append_tag_to_element(th.CONSTANT_BOX_NAMES.components, components_element)
+    th.append_tag_to_element(th.CONSTANT_BOX_NAMES.duration, duration_element)
+    th.append_tag_to_element(th.CONSTANT_BOX_NAMES.spell_range, distance_element)
+    th.append_tag_to_element(th.CONSTANT_BOX_NAMES.casting_time , casting_time_element)
+    th.append_tag_to_element(th.CONSTANT_BOX_NAMES.description , description_element)
 
     th.render()
 
--- template_handler.py
+++ template_handler.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+from typing import Any
+from bs4 import BeautifulSoup, Tag
 from bs4.element import TemplateString
 from html2image import Html2Image
 import shutil
@@ -10,26 +11,55 @@ DEFAULT_OUTP_NAME = "build/outp.png"
 DEFAULT_SIZE = (408, 700)
 
 class TemplateHandler:
-    soup: BeautifulSoup
-    components: TemplateString
-    casting_time: TemplateString
-    spell_range: TemplateString
-    duration: TemplateString
-    description: TemplateString
-    template: str
     screenshot_options: dict = {
             "html_file": 'build/template/outp.html',
             "css_file": 'build/template/style.css',
             "save_as": 'outp.png',
             "size": (408, 700)
             }
+    class CONSTANT_BOX_NAMES:
+        components: str = "components"
+        casting_time: str = "casting-time"
+        spell_range: str = "range"
+        duration: str = "duration"
+        description: str = "description"
+        spell_info:str = "spell_info"
+        spell_name: str = "spell_name"
+
+
+    class ParsedStrings:
+        def __init__(self) -> None:
+            cbn: TemplateHandler.CONSTANT_BOX_NAMES = TemplateHandler.CONSTANT_BOX_NAMES()
+            self.__raw_dict: dict= {
+                    cbn.components: None,
+                    cbn.casting_time: None,
+                    cbn.spell_range: None,
+                    cbn.duration: None,
+                    cbn.description: None,
+                    cbn.spell_info: None,
+                    cbn.spell_name: None
+                    }
+        def __getitem__(self, name: str, /) -> TemplateString | None:
+            return self.__raw_dict[name]
+
+        def __setitem__(self, name: str, value: TemplateString | None, /) -> None:
+            if name not in self.__raw_dict.keys():
+                raise Exception
+            self.__raw_dict[name] = value
+
+        def keys(self) -> list:
+            return list(self.__raw_dict.keys())
+
 
     def __init__(self, template_name: str | None = None) -> None:
         if not template_name:
             template_name = DEFAULT_TEMPLATE
-        self.template = template_name
+        self.soup: BeautifulSoup
+        self.parsed_strings: TemplateHandler.ParsedStrings =  self.ParsedStrings()
+        self.template: str = template_name
         self.get_soup()
-        self.populate_box_vars()
+        self.populate_parsed_strings()
+
 
     def render(self, size: tuple[int, int] = DEFAULT_SIZE, file_path: str = DEFAULT_OUTP_NAME) -> None:
         self.screenshot_options['size'] = size
@@ -46,37 +76,36 @@ class TemplateHandler:
 
 
 
-    class CONSTANT_BOX_NAMES:
-        components: str = "components"
-        casting_time: str = "casting-time"
-        spell_range: str = "range"
-        duration: str = "duration"
-        description: str = "description"
 
-    def populate_box_vars(self) -> None:
-        components_list = self.find_elements_by_box(self.CONSTANT_BOX_NAMES.components)
-        casting_time_list = self.find_elements_by_box(self.CONSTANT_BOX_NAMES.casting_time)
-        spell_range_list = self.find_elements_by_box(self.CONSTANT_BOX_NAMES.spell_range)
-        duration_list = self.find_elements_by_box(self.CONSTANT_BOX_NAMES.duration)
-        description_list = self.find_elements_by_box(self.CONSTANT_BOX_NAMES.description)
-        for i in [components_list, casting_time_list, spell_range_list, duration_list]:
-            if len(i) > 1:
-                print(f"More than 1 box of same type found in {self.template}!")
-                print(i)
-                exit(1)
-        self.components=components_list[0]
-        self.casting_time=casting_time_list[0]
-        self.spell_range=spell_range_list[0]
-        self.duration=duration_list[0]
-        self.description=description_list[0]
+    def populate_parsed_strings(self) -> None:
+        for key in self.parsed_strings.keys():
+            self.parsed_strings[key] = self.find_elements_by_box(key)
 
-    def find_elements_by_box(self, box_value: str) -> list:
-        return self.soup.find_all(box= box_value)
+    def append_tag_to_element(self, element_name: str, new_tag: Tag) -> None:
+        element = self.parsed_strings[element_name]
+        if element:
+            element.append(new_tag)
+        else:
+            print(f'Element with name {element_name} is not found in template, skipping')
+
+
+
+    def find_elements_by_box(self, box_value: str) -> TemplateString | None:
+        found_boxes: list[TemplateString] = self.soup.find_all(box= box_value)
+        if len(found_boxes) > 1:
+            print(f"More than 1 box of same type found in {self.template}!")
+            print(found_boxes)
+            exit(1)
+        if found_boxes:
+            return found_boxes[0]
+        return None
 
     def prepare_build(self, template_name: str | None):
         if os.path.exists('build/template'):
             os.remove('build/template')
         os.symlink(f"../templates/{template_name}", 'build/template', True)
+        if not os.path.exists('build/src'):
+            os.symlink('template/src', 'build/template', True)
 
     def render_html(self):
         with open('build/template/outp.html', 'wb') as outp_file:
@@ -88,11 +117,39 @@ class TemplateHandler:
         #         'width':408,
         #         'height':700,
         #         }
+        def recursive_find_files(directory_path: str) -> list[str] :
+            file_list: list[str] = []
+            files = os.listdir(directory_path)
+            for file in files:
+                realfile = directory_path + '/'+  file
+                if os.path.isdir(realfile):
+                    file_list.extend(recursive_find_files(realfile))
+                else:
+                    file_list.append(realfile)
+
+            return file_list
+
         file_path: str = self.screenshot_options['save_as']
         file_name = file_path[file_path.find('/')+1:]
         self.screenshot_options['save_as'] = file_name
+        # self.screenshot_options['css_file'] = 'build/template/style_1.css'
+        build_dir_path = os.path.realpath('build/template')
+        file_list: list[str] = recursive_find_files(os.path.realpath(build_dir_path))
 
-        hti = Html2Image()
+        hti = Html2Image(temp_path='build')
+        # hti: Html2Image = Html2Image()
+        # for file in file_list:
+        #     dot_index: int = ''.join(reversed(file)).find('.')
+        #     if dot_index == -1:
+        #         continue
+        #     file_extension: str = file[-dot_index-1:]
+        #     if file_extension == ".svg" or file_extension == ".png":
+        #         hti.load_file(file)
+            
+        # hti.load_file(os.path.realpath('/home/lunaro/git/dnd-card-spell-template/templates/basic-template/src/ritual.png'))
+
+
+
         hti.screenshot(**self.screenshot_options)
         shutil.move(file_name, file_path)
 
