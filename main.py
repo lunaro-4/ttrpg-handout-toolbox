6301b61881bbcb74d26c893d501a66c97745f9c5
lunaro <pavelike@gmail.com>
2025-02-14 15:25:08 +0300

    add example usage to main file

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 # from template_handler import TemplateHandler
 # from spell_data_handler import Spell
+from global_constants import BasicTemplateColorschemes
 
 def main():
+    """ Usage examples:
+sdb = SpellDatabase(directory='spell_data_from_dndsu')
+sdb.process_spells()
+custom_css = BasicTemplateColorschemes.druid_colorscheme
+
+SpellDatabase.render_spells_to_folder('build/wizard', *sdb.class_to_spells[CLASS_SUBCLASS_MAP.wizard_class_name], restrict_to=3, custom_css=custom_css)
+# SpellDatabase.combine_images_to_printable('build/wizard', 'build/bulk')
+
+
+# print(type(RussianTranslations.SPELL_LEVELS[0]))
+# print(type(RussianTranslations.SPELL_LEVELS[1]))
+# print(type(RussianTranslations.SPELL_LEVELS[2]))
+
+# dsp = DndSuParser()
+# dsp.populate_spells_list_from_file('spells.html')
+# dsp.link_names_to_files('spells_raw_html')
+# dsp.populate_soups_from_files()
+# dsp.process_spells()
+# # print(dsp.spells)
+# dsp.save_spells('spell_data_from_dndsu')
+# print(dsp.spells_raw[0])
+"""
     return
 
 
